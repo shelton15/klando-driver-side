db337372c9e848d1f5cb6203c54cdbb4ecd813ad
shelton15 <sheltoniyassa@gmail.com>
2022-10-10 10:42:44 +0100

    .

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:firebase_core/firebase_core.dart';
 
 import 'package:firebase_phone_auth_handler/firebase_phone_auth_handler.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 import 'screens/lottiescreen.dart';
 
--- lib/screens/phone_field.dart
+++ lib/screens/phone_field.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:intl_phone_field/intl_phone_field.dart';
 
 class PhoneField extends StatefulWidget {
   const PhoneField({Key? key}) : super(key: key);
@@ -11,29 +10,27 @@ class PhoneField extends StatefulWidget {
 class _PhoneFieldState extends State<PhoneField> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: SafeArea(
-          top: true,
-          child: Scaffold(
-            body: Column(
-              children: [
-                Center(
-                    child: Padding(
-                  padding: const EdgeInsets.only(top: 100, bottom: 10),
-                  child: Image.asset('assets/Cardrivingvro1.png'),
-                )),
-                Center(
-                  child: Text(
-                    'Enter Your Phone Number',
-                    style: TextStyle(
-                        fontWeight: FontWeight.normal,
-                        fontSize: 15,
-                        color: Theme.of(context).colorScheme.secondary),
-                  ),
-                )
-              ],
-            ),
-          )),
-    );
+    return SafeArea(
+        top: true,
+        child: Scaffold(
+          body: Column(
+            children: [
+              Center(
+                  child: Padding(
+                padding: const EdgeInsets.only(top: 100, bottom: 10),
+                child: Image.asset('assets/Cardrivingvro1.png'),
+              )),
+              Center(
+                child: Text(
+                  'Enter Your Phone Number',
+                  style: TextStyle(
+                      fontWeight: FontWeight.normal,
+                      fontSize: 15,
+                      color: Theme.of(context).colorScheme.secondary),
+                ),
+              )
+            ],
+          ),
+        ));
   }
 }
